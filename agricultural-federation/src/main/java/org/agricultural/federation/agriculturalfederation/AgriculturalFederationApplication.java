33c6864eb967b6c7593cb4857914a5390d269fad
mathieu <mathieu.user24@gmail.com>
2026-04-21 15:17:02 +0300

    feat: Implement Spring-boot application with agricultural federation api - v0.0.1.yaml

---
+++ agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/AgriculturalFederationApplication.java
@@ -0,0 +1,13 @@
+package org.agricultural.federation.agriculturalfederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalFederationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalFederationApplication.class, args);
+    }
+
+}
---
+++ agricultural-federation/src/test/java/org/agricultural/federation/agriculturalfederation/AgriculturalFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.agricultural.federation.agriculturalfederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
