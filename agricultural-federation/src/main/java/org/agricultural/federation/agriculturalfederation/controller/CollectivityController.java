098c0922a85399012c08b3d56139201e3f7d1356
mathieu <mathieu.user24@gmail.com>
2026-04-22 09:48:58 +0300

    feat: change query parameter to requestBody

--- agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/controller/CollectivityController.java
+++ agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/controller/CollectivityController.java
@@ -1,5 +1,6 @@
 package org.agricultural.federation.agriculturalfederation.controller;
 
+import org.agricultural.federation.agriculturalfederation.entity.CollectivityIdentifier;
 import org.agricultural.federation.agriculturalfederation.entity.CreateCollectivity;
 import org.agricultural.federation.agriculturalfederation.exception.BadRequestException;
 import org.agricultural.federation.agriculturalfederation.exception.NotFoundException;
@@ -37,13 +38,14 @@ public class CollectivityController {
 
     @PutMapping("/collectivities")
     public ResponseEntity<?> generateIdentifier(
-            @RequestParam(required = false) Integer numero,
-            @RequestParam(required = false) String name
+            @RequestBody CollectivityIdentifier collectivityIdentifier
     ) {
         try {
+            Integer number = collectivityIdentifier.getNumber();
+            String name = collectivityIdentifier.getName();
             return ResponseEntity
                     .status(200)
-                    .body(collectivityService.generateIdentifier(numero, name));
+                    .body(collectivityService.generateIdentifier(number, name));
         } catch (BadRequestException e) {
             return ResponseEntity
                     .status(400)
