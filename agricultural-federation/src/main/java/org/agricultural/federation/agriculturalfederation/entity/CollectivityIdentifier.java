778a1b86695b5da0f92b73c2606011b4600c2e9e
mathieu <mathieu.user24@gmail.com>
2026-04-22 08:54:40 +0300

    feat: create class CollectivityIdentifier

---
+++ agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/entity/CollectivityIdentifier.java
@@ -0,0 +1,27 @@
+package org.agricultural.federation.agriculturalfederation.entity;
+
+public class CollectivityIdentifier {
+    private Integer numero;
+    private String name;
+
+    public CollectivityIdentifier(Integer numero, String name) {
+        this.numero = numero;
+        this.name = name;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
