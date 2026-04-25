3a39d0afc7a1162df4b0fa9f69f06d46f0abd9b9
mathieu <mathieu.user24@gmail.com>
2026-04-25 20:39:44 +0300

    feat: reImplement RowMapper

--- agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/mapper/RowMapper.java
+++ agricultural-federation/src/main/java/org/agricultural/federation/agriculturalfederation/mapper/RowMapper.java
@@ -1,41 +1,42 @@
 package org.agricultural.federation.agriculturalfederation.mapper;
 
-import org.agricultural.federation.agriculturalfederation.entity.Collectivity;
-import org.agricultural.federation.agriculturalfederation.entity.Gender;
-import org.agricultural.federation.agriculturalfederation.entity.Member;
-import org.agricultural.federation.agriculturalfederation.entity.PaymentMode;
+import org.agricultural.federation.agriculturalfederation.entity.*;
 import org.springframework.stereotype.Component;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Component
 public class RowMapper {
 
     public Collectivity mapToCollectivity(ResultSet rs) throws SQLException {
-        Collectivity c = new Collectivity();
-        c.setId(rs.getInt("id"));
-        c.setNumber(rs.getInt("number"));
-        c.setName(rs.getString("name"));
-        c.setLocation(rs.getString("location"));
-        c.setSpeciality(rs.getString("speciality"));
-        c.setCreationDate(rs.getTimestamp("created_at").toInstant());
-        c.setFederationApproval(rs.getBoolean("federation_approval"));
-        return c;
+        return new Collectivity(
+                rs.getString("id"),
+                rs.getInt("number"),
+                rs.getString("name"),
+                rs.getString("location"),
+                rs.getString("speciality"),
+                null,
+                null,
+                (rs.getObject("updated_at") == null ? null
+                        : rs.getTimestamp("updated_at").toInstant())
+        );
+//        c.setFederationApproval(rs.getBoolean("federation_approval"));
     }
 
     public Member mapToMember(ResultSet rs) throws SQLException {
         return new Member(
-                rs.getInt("id"),
+                rs.getString("member_id"),
                 rs.getString("first_name"),
                 rs.getString("last_name"),
-                rs.getDate("birth_date").toLocalDate().atStartOfDay().toInstant(java.time.ZoneOffset.UTC),
+                rs.getDate("birthdate"),
                 Gender.valueOf(rs.getString("gender")),
                 rs.getString("address"),
                 rs.getString("profession"),
                 rs.getString("phone_number"),
                 rs.getString("email"),
-                rs.getDate("adhesion_date").toLocalDate().atStartOfDay().toInstant(java.time.ZoneOffset.UTC)
+                MemberOccupation.valueOf(rs.getString("occupation"))
         );
     }
 
@@ -43,8 +44,39 @@ public class RowMapper {
         return switch (dbValue) {
             case "CASH" -> PaymentMode.CASH;
             case "BANK_TRANSFER" -> PaymentMode.BANK_TRANSFER;
-            case "MOBILE_MONEY" -> PaymentMode.MOBILE_BANKING;
+            case "MOBILE_MONEY" -> PaymentMode.MOBILE_MONEY;
             default -> throw new IllegalArgumentException("Unknown payment method: " + dbValue);
         };
+    }
+
+    public MembershipFee mapToMembershipFee(ResultSet rs) throws SQLException {
+        MembershipFee membershipFee = new MembershipFee();
+        membershipFee.setEligibleFrom(rs.getDate("eligible_from"));
+        membershipFee.setFrequency(rs.getString("frequency"));
+        membershipFee.setAmount(rs.getDouble("amount"));
+        membershipFee.setLabel(rs.getString("label"));
+        membershipFee.setId(rs.getString("id"));
+        membershipFee.setStatus(Status.valueOf(rs.getString("status")));
+        return membershipFee;
+    }
+
+    public Transaction mapToTransaction(ResultSet rs, FinancialAccount financialAccount) throws SQLException {
+        Transaction t = new Transaction();
+        t.setId(rs.getInt("id"));
+        t.setCreationDate(rs.getTimestamp("created_at").toInstant());
+        t.setAmount(rs.getDouble("amount"));
+        t.setPaymentMode(PaymentMode.valueOf(rs.getString("payment_mode")));
+        t.setAccountCredited(financialAccount);
+        t.setMemberDebited(mapToMember(rs));
+        return t;
+    }
+
+    public CollectivityStructure mapToCollectivityStructure(List<Member> members) {
+        return new CollectivityStructure(
+                members.stream().filter(x -> x.getOccupation() == MemberOccupation.PRESIDENT).findFirst().orElse(null),
+                members.stream().filter(x -> x.getOccupation() == MemberOccupation.VICE_PRESIDENT).findFirst().orElse(null),
+                members.stream().filter(x -> x.getOccupation() == MemberOccupation.TREASURER).findFirst().orElse(null),
+                members.stream().filter(x -> x.getOccupation() == MemberOccupation.SECRETARY).findFirst().orElse(null)
+        );
     }
 }
